f17ef2adf6a172be0864a66fd3c1d9f7bc2b141d
Kyle Fuller <kyle@fuller.li>
2015-10-31 10:22:46 -0700

    [Conche] Update 0.5.0

--- Formula/conche.rb
+++ Formula/conche.rb
@@ -1,9 +1,8 @@
 class Conche < Formula
   homepage 'https://github.com/kylef/Conche'
-  head 'https://github.com/kylef/Conche.git', :tag => '0.4.0'
+  head 'https://github.com/kylef/Conche.git', :tag => '0.5.0'
 
   def install
     system 'make', "DESTDIR=#{prefix}", 'install'
   end
 end
-
