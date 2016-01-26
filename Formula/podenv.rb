fd5ce9ec736c4dd6863e30de3f25c23b24547d30
Kyle Fuller <kyle@fuller.li>
2016-01-26 20:59:23 +0000

    [Add] podenv 1.0.0

--- Formula/podenv.rb
+++ Formula/podenv.rb
@@ -1,8 +1,8 @@
 class Podenv < Formula
   desc "CocoaPods version manager"
   homepage "https://github.com/kylef/podenv"
-  url "https://github.com/kylef/podenv/archive/0.1.0.tar.gz"
-  sha256 "760a5c5471341406a51978bbbb1d3b8e516555dc1ed594af3abd66675afb6bb2"
+  url "https://github.com/kylef/podenv/archive/1.0.0.tar.gz"
+  sha256 "952dd7d93c22d3b4d6de27c4325b27e3387e2abf4075749bc36a855dba1c937c"
   head "https://github.com/kylef/podenv.git"
 
   def install
