ffb01b513520d798c4e36049d6fe5442bb911e40
Kyle Fuller <kyle@fuller.li>
2016-02-02 00:07:17 +0000

    [Add] swiftenv 0.5.0

--- Formula/swiftenv.rb
+++ Formula/swiftenv.rb
@@ -1,8 +1,8 @@
 class Swiftenv < Formula
   desc "Swift version manager"
   homepage "https://github.com/kylef/swiftenv"
-  url "https://github.com/kylef/swiftenv/archive/0.4.0.tar.gz"
-  sha256 "e5009d6811d73983b9170c5d359d5661ec23f0b94c056b3bc6e1e24ec924cadc"
+  url "https://github.com/kylef/swiftenv/archive/0.5.0.tar.gz"
+  sha256 "74651893e18c7d2ef35de4cfa0a9e91ccae592ef932e02b41eacbafad1ece0e6"
   head "https://github.com/kylef/swiftenv.git"
 
   def install
